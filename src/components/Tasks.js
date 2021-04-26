c6d0274eb87ec59af62963f657ea36691b1abcd5
agn2017331008 <ghosharpita423@gmail.com>
2021-04-26 10:10:01 +0600

    Adds ALL

---
+++ src/components/Tasks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Task from './Task'
+
+const Tasks = (props) => {
+    
+    return (
+        <>
+            {props.tasks.map((task , index) => (
+                <Task key={index} task={task}
+                onDelete = {props.onDelete}
+                onToggle = {props.onToggle}/>
+            ))}
+        </>
+    )
+}
+
+export default Tasks
